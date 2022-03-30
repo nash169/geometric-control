3091a80c753a5e4d2de70788510aff91a4c0294b
bernardo <bernardo.fichera@gmail.com>
2022-03-31 00:29:43 +0200

    Testing projection of gradient and hessian.

--- scripts/bundle_ds.py
+++ scripts/bundle_ds.py
@@ -1,10 +1,14 @@
 #! /usr/bin/env python
 # encoding: utf-8
 
+from shutil import which
+from turtle import color
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+from scipy.linalg import eig
+from scipy.sparse.linalg import eigs
 from matplotlib import cm, colors
 from io_utils import get_data
 
@@ -27,51 +31,60 @@ surf = ax.plot_surface(
     embedding[:, 2].reshape((res, res), order='F'),
     facecolors=cm.jet(function.reshape(
         (res, res), order='F')/np.amax(function)),
-    antialiased=False, linewidth=0, alpha=0.3
+    antialiased=False, linewidth=0, alpha=1
 )
 fig.colorbar(surf, ax=ax)
 
-# trajectory
-ax.plot(ds[:, 1], ds[:, 2], ds[:, 3], color="black")
-# target
-ax.scatter(0.0707372, -0.987513,  0.140767, color="red")
-# init pos
+# # trajectory
+# ax.plot(ds[:, 1], ds[:, 2], ds[:, 3], color="black")
+# # target
+# ax.scatter(0.0707372, -0.987513,  0.140767, color="red")
+# # init pos
 ax.scatter(ds[0, 1], ds[0, 2], ds[0, 3], color="black")
-# end pos
-ax.scatter(ds[-1, 1], ds[-1, 2], ds[-1, 3], color="blue")
+# # end pos
+# ax.scatter(ds[-1, 1], ds[-1, 2], ds[-1, 3], color="blue")
 # init vel
 ax.quiver(ds[0, 1], ds[0, 2], ds[0, 3], ds[0, 4],
-          ds[0, 5], ds[0, 6], length=0.25)
+          ds[0, 5], ds[0, 6], length=100, color='k')
 
-# OBSTAClE
-r = 0.4
-c = np.array([0.362358, -0.872814, -0.326944])
-u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-x = c[0] + r*np.cos(u)*np.sin(v)
-y = c[1] + r*np.sin(u)*np.sin(v)
-z = c[2] + r*np.cos(v)
-ax.plot_surface(x, y, z, linewidth=0.0, cstride=1, rstride=1)
+x = np.array([ds[0, 1], ds[0, 2], ds[0, 3]])
+P = np.eye(3, 3) - x * x.T
+
+vals, vecs = eigs(P, k=2, which='LM')
+print(vals)
+ax.quiver(x[0], x[1], x[2], vecs[0, 0], vecs[1, 0], vecs[2, 0], color='r')
+ax.quiver(x[0], x[1], x[2], vecs[0, 1], vecs[1, 1], vecs[2, 1], color='g')
+ax.quiver(x[0], x[1], x[2], vecs[0, 2], vecs[1, 2], vecs[2, 2], color='b')
+
+# # OBSTAClE
+# r = 0.4
+# c = np.array([0.362358, -0.872814, -0.326944])
+# u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+# x = c[0] + r*np.cos(u)*np.sin(v)
+# y = c[1] + r*np.sin(u)*np.sin(v)
+# z = c[2] + r*np.cos(v)
+# ax.plot_surface(x, y, z, linewidth=0.0, cstride=1, rstride=1)
 
 
-# DYNAMICS
-fig = plt.figure()
-ax = fig.add_subplot(121)
-ax.plot(ds[:, 0], ds[:, 1])
-ax.plot(ds[:, 0], ds[:, 2])
-ax.plot(ds[:, 0], ds[:, 3])
-ax.hlines(0.0707372, np.min(ds[:, 0]), np.max(ds[:, 0]),
-          color="black", linestyles="dashed")
-ax.hlines(-0.987513, np.min(ds[:, 0]), np.max(ds[:, 0]),
-          color="black", linestyles="dashed")
-ax.hlines(0.140767, np.min(ds[:, 0]), np.max(ds[:, 0]),
-          color="black", linestyles="dashed")
+# # DYNAMICS
+# fig = plt.figure()
+# ax = fig.add_subplot(121)
+# ax.plot(ds[:, 0], ds[:, 1])
+# ax.plot(ds[:, 0], ds[:, 2])
+# ax.plot(ds[:, 0], ds[:, 3])
+# ax.hlines(0.0707372, np.min(ds[:, 0]), np.max(ds[:, 0]),
+#           color="black", linestyles="dashed")
+# ax.hlines(-0.987513, np.min(ds[:, 0]), np.max(ds[:, 0]),
+#           color="black", linestyles="dashed")
+# ax.hlines(0.140767, np.min(ds[:, 0]), np.max(ds[:, 0]),
+#           color="black", linestyles="dashed")
 
-ax = fig.add_subplot(122)
-ax.plot(ds[:, 0], ds[:, 4])
-ax.plot(ds[:, 0], ds[:, 5])
-ax.plot(ds[:, 0], ds[:, 6])
-ax.hlines(0.0, np.min(ds[:, 0]), np.max(ds[:, 0]),
-          color="black", linestyles="dashed")
+# ax = fig.add_subplot(122)
+# ax.plot(ds[:, 0], ds[:, 4])
+# ax.plot(ds[:, 0], ds[:, 5])
+# ax.plot(ds[:, 0], ds[:, 6])
+# ax.hlines(0.0, np.min(ds[:, 0]), np.max(ds[:, 0]),
+#           color="black", linestyles="dashed")
 
 
 plt.show()
--- src/examples/bundle_ds.cpp
+++ src/examples/bundle_ds.cpp
@@ -24,6 +24,9 @@ class ManifoldsMapping : public TreeManifoldsImpl {
     Eigen::VectorXd map(const Eigen::VectorXd& x, manifolds::Sphere<2>& manifold) override { return Eigen::VectorXd::Zero(x.size()); }
     Eigen::MatrixXd jacobian(const Eigen::VectorXd& x, manifolds::Sphere<2>& manifold) override { return Eigen::MatrixXd::Zero(x.size(), x.size()); }
     Eigen::MatrixXd hessian(const Eigen::VectorXd& x, const Eigen::VectorXd& v, manifolds::Sphere<2>& manifold) override { return Eigen::MatrixXd::Zero(x.size(), x.size()); }
+
+public:
+    ParentManifold* _manifold;
 };
 
 // Parent Manifold map specialization
@@ -52,64 +55,72 @@ int main(int argc, char** argv)
     // Create DS on a specific manifold
     dynamics::BundleDynamics<Manifold, TreeManifoldsImpl, ManifoldsMapping> ds;
 
-    // // Assign potential and dissipative task to the DS
-    // ds.addTasks(
-    //     std::make_unique<tasks::PotentialEnergy<Manifold>>(),
-    //     std::make_unique<tasks::DissipativeEnergy<Manifold>>(),
-    //     std::make_unique<tasks::ObstacleAvoidance<Manifold>>());
+    // Assign potential and dissipative task to the DS
+    ds.addTasks(
+        std::make_unique<tasks::PotentialEnergy<Manifold>>(),
+        std::make_unique<tasks::DissipativeEnergy<Manifold>>(),
+        std::make_unique<tasks::ObstacleAvoidance<Manifold>>());
 
-    // // Attractor
-    // Eigen::Vector3d a = Manifold().embedding(Eigen::Vector2d(1.5, 3));
+    // Attractor
+    Eigen::Vector3d a = Manifold().embedding(Eigen::Vector2d(1.5, 3));
 
-    // // Set tasks' properties
-    // static_cast<tasks::PotentialEnergy<Manifold>&>(ds.task(0)).setStiffness(Eigen::Matrix3d::Identity()).setAttractor(a);
-    // static_cast<tasks::DissipativeEnergy<Manifold>&>(ds.task(1)).setDissipativeFactor(10 * Eigen::Matrix3d::Identity());
-    // static_cast<tasks::ObstacleAvoidance<Manifold>&>(ds.task(2)).setRadius(0.4).setCenter(Eigen::Vector2d(1.2, 3.5)).setMetricParams(1, 1);
+    // Set tasks' properties
+    static_cast<tasks::PotentialEnergy<Manifold>&>(ds.task(0)).setStiffness(Eigen::Matrix3d::Identity()).setAttractor(a);
+    static_cast<tasks::DissipativeEnergy<Manifold>&>(ds.task(1)).setDissipativeFactor(10 * Eigen::Matrix3d::Identity());
+    static_cast<tasks::ObstacleAvoidance<Manifold>&>(ds.task(2)).setRadius(0.4).setCenter(Eigen::Vector2d(1.2, 3.5)).setMetricParams(1, 1);
 
-    // // Embedding
-    // Eigen::VectorXd potential(num_samples);
-    // Eigen::MatrixXd embedding(num_samples, dim + 1);
+    // Embedding
+    Eigen::VectorXd potential(num_samples);
+    Eigen::MatrixXd embedding(num_samples, dim + 1);
 
-    // for (size_t i = 0; i < num_samples; i++) {
-    //     embedding.row(i) = Manifold().embedding(X.row(i));
-    //     potential(i) = ds.task(0).map(embedding.row(i))[0];
-    // }
+    for (size_t i = 0; i < num_samples; i++) {
+        embedding.row(i) = Manifold().embedding(X.row(i));
+        potential(i) = ds.task(0).map(embedding.row(i))[0];
+    }
 
-    // // Dynamics
-    // double time = 0, max_time = 30, dt = 0.001;
-    // size_t num_steps = std::ceil(max_time / dt) + 1, index = 0;
-    // Eigen::Vector3d x = Manifold().embedding(Eigen::Vector2d(0.7, 5)),
-    //                 v = Manifold().projector(x, (Manifold().embedding(Eigen::Vector2d(1.5, 3)) - x) * 0.01);
-    // // v = Manifold().jacobian(Eigen::Vector2d(1, 4)) * Eigen::Vector2d(-1, 1);
+    // Dynamics
+    double time = 0, max_time = 30, dt = 0.001;
+    size_t num_steps = std::ceil(max_time / dt) + 1, index = 0;
+    Eigen::Vector3d x = Manifold().embedding(Eigen::Vector2d(0.7, 5)),
+                    v = Manifold().project(x, (Manifold().embedding(Eigen::Vector2d(1.5, 3)) - x) * 0.01);
+    // v = Manifold().jacobian(Eigen::Vector2d(1, 4)) * Eigen::Vector2d(-1, 1);
 
-    // // Record
-    // Eigen::MatrixXd record = Eigen::MatrixXd::Zero(num_steps, 1 + 2 * (dim + 1));
-    // record.row(0)(0) = time;
-    // record.row(0).segment(1, dim + 1) = x;
-    // record.row(0).segment(dim + 2, dim + 1) = v;
+    std::cout << "Initial position" << std::endl;
+    std::cout << x.transpose() << std::endl;
+    std::cout << "Initial velocity" << std::endl;
+    std::cout << v.transpose() << std::endl;
+    std::cout << "Initial acceleration" << std::endl;
+    std::cout << ds(x, v).transpose() << std::endl;
+
+    // Record
+    Eigen::MatrixXd record = Eigen::MatrixXd::Zero(num_steps, 1 + 2 * (dim + 1));
+    record.row(0)(0) = time;
+    record.row(0).segment(1, dim + 1) = x;
+    record.row(0).segment(dim + 2, dim + 1) = v;
 
     // std::cout << Manifold().embedding(Eigen::Vector2d(1.2, 3.5)).transpose() << std::endl;
 
-    // while (time < max_time && index < num_steps - 1) {
-    //     // Velocity
-    //     v = v + dt * Manifold().projector(x, ds(x, v));
+    while (time < max_time && index < num_steps - 1) {
+        // Velocity
+        v = v + dt * Manifold().project(x, ds(x, v));
+        // v = v + dt * Manifold().project(x, ds.update(x, v).solve()._ddx);
 
-    //     // Position
-    //     x = Manifold().retraction(x, v, dt);
+        // Position
+        x = Manifold().retract(x, v, dt);
 
-    //     // Step forward
-    //     time += dt;
-    //     index++;
+        // Step forward
+        time += dt;
+        index++;
 
-    //     // Record
-    //     record.row(index)(0) = time;
-    //     record.row(index).segment(1, dim + 1) = x;
-    //     record.row(index).segment(dim + 2, dim + 1) = v;
-    // }
+        // Record
+        record.row(index)(0) = time;
+        record.row(index).segment(1, dim + 1) = x;
+        record.row(index).segment(dim + 2, dim + 1) = v;
+    }
 
-    // FileManager io_manager;
-    // io_manager.setFile("rsc/sphere_bundle.csv");
-    // io_manager.write("RECORD", record, "EMBEDDING", embedding, "POTENTIAL", potential);
+    FileManager io_manager;
+    io_manager.setFile("rsc/sphere_bundle.csv");
+    io_manager.write("RECORD", record, "EMBEDDING", embedding, "POTENTIAL", potential);
 
     return 0;
 }
